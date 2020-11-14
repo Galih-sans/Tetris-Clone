4f830fd8973f990ed7dee726626a06f6de66e3d2
Galih Krisna Aji <43230707+Racoonanoda@users.noreply.github.com>
2020-11-14 14:06:33 +0700

    First commit
    Adding all file from unity project

---
+++ Tutrus/Tutrus/Assets/Scripts/SpawnTetromino.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTetromino : MonoBehaviour
+{
+    public GameObject[] Tetrominos;
+    void Start()
+    {
+        NewTetromino();
+    }
+
+    public void NewTetromino()
+    {
+        Instantiate(Tetrominos[Random.Range(0, Tetrominos.Length)], transform.position, Quaternion.identity);
+    }
+}
